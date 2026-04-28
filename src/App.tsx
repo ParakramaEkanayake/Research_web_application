9c8380d3bce1375662d87dd9eea538f8f5bceba7
Parakrama Ekanayake <it22267122@my.sliit.lk>
2026-04-28 14:55:07 +0530

    22

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    // Redirect to the static HTML website
+  // Only redirect if we aren't already there to prevent loops
+  if (window.location.pathname !== "/index.html") {
     window.location.href = "/index.html";
-  }, []);
+  }
+}, []);
 
   return null;
 }
